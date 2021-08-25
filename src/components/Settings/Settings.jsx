0dd253e24841306abf229c7dea8e6dd9b8abc6cf
VladyslavHimself <vladyslavhimself@gmail.com>
2021-08-25 14:13:51 -0400

    added todo comments for settings

--- src/components/Settings/Settings.jsx
+++ src/components/Settings/Settings.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
 
+import classes from './Settings.module.scss';
+
+
+  /**
+   * 
+   * @param {*} props 
+   * @returns 
+   * 
+   * @Must_Consists
+   * 
+   * @Configurations 
+   * TODO: Focus time / Pause Time (short / long) changer, 
+   *
+   * 
+   */
+
 export default function Settings(props) {
   return (
     <h1>Settings</h1>
